7ee95951779ac10228cd3c90f4c59a46cf5113de
Varun M <muthukumarvarun@gmail.com>
2023-07-18 22:41:43 +0530

    Routers implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import Checkout from './Checkout';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      < Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/Checkout" element={[<Header />,<Checkout />]}/>
+          <Route path="/" element={[<Header />,<Home />]}/>
+        </Routes>
+      </div>
+    </Router>  
   );
 }
 
